87fc5f02927d828ee4cd6ac562fd901dee7a51b0
Marickyare <83879224+Marickyare@users.noreply.github.com>
2024-06-07 21:30:42 +0300

    Update task2.cpp

--- task2.cpp
+++ task2.cpp
@@ -1,16 +1,16 @@
-﻿#include <locale.h>
+#include <locale.h>
 #include <iostream>
 
 using namespace std;
 
 int main() {
 	setlocale(LC_ALL, "Russian");
-	int x, y;
+	float x, y;
 	cout << "Введите значение x: "; cin >> x;
 	cout << "Введите значение y: "; cin >> y;
 
-	if (sqrt(x*x + y*y) >= 1 & sqrt(x*x + y*y) <= 2) {
-		if (x >= 0 & x <= 2) {
+	if (x > -1 & x < 1 & y > -1 & y < 1) {
+		if (((-1) * x - y) > 0){
 			cout << "Точка подходит";
 		}
 		cout << "Точка не подходит";
