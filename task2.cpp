4745eb4d255b9f7b6ba2c630f41c8a4fdafff6e7
Marickyare <83879224+Marickyare@users.noreply.github.com>
2024-06-07 20:38:01 +0300

    Add files via upload

---
+++ task2.cpp
@@ -0,0 +1,19 @@
+﻿#include <locale.h>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int x, y;
+	cout << "Введите значение x: "; cin >> x;
+	cout << "Введите значение y: "; cin >> y;
+
+	if (sqrt(x*x + y*y) >= 1 & sqrt(x*x + y*y) <= 2) {
+		if (x >= 0 & x <= 2) {
+			cout << "Точка подходит";
+		}
+		cout << "Точка не подходит";
+	}
+	cout << "Точка не подходит";
+}
