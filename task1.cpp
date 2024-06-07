7cad62eabde5f67d05664c2c962d7b68d8765396
Marickyare <83879224+Marickyare@users.noreply.github.com>
2024-06-07 20:29:25 +0300

    Add files via upload

---
+++ task1.cpp
@@ -0,0 +1,17 @@
+﻿#define M_PI 3.14159265358979323846
+#include <math.h>
+#include <iostream>
+
+
+using namespace std;
+
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	float a, b;
+	cout << "Ввежите значение b: "; cin >> b;
+		cout << "Ввежите значение a: "; cin >> a;
+		float F = ((0.75 * sqrt(b) - 1 / 2 * pow(a, 1 / 3)) * sin(b * M_PI)) / cos(pow((a + b), 2) + exp(a));
+		cout << F;
+
+}
