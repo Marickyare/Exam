05ac704ef3b8d639c26fcd30e60e71b614b4a854
Marickyare <83879224+Marickyare@users.noreply.github.com>
2024-06-08 12:29:55 +0300

    Add files via upload

---
+++ task3.cpp
@@ -0,0 +1,23 @@
+﻿#include <locale.h>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+
+	int n = 1;
+	float E;
+	float summ = n / pow((n + 1), n);
+	n++;
+	cout << "Введите значение Е: "; cin >> E;
+	while ((n / pow((n + 1), n)) - ((n - 1) / pow(((n - 1) + 1), n - 1)) > E) {
+		if ((n / pow((n + 1), n) ) < E) {
+			summ += (n / pow((n + 1), n));
+			n++;
+			cout << "Значение подходит.";
+		}
+	}
+	cout << "Значение не подходит.";
+}
+
