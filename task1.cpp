bb37096ce3d486aa3f112b286082d0be66ec485c
Marickyare <83879224+Marickyare@users.noreply.github.com>
2024-06-08 13:42:25 +0300

    Add files via upload

---
+++ task1.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <locale.h>
+#include <fstream>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+
+	ifstream Rf;
+	Rf.open("R.txt");
+
+	if (!Rf) {
+		cout << "Не удалось открыть файл.";
+		return 1;
+	}
+
+	int R;
+	Rf >> R;
+	Rf.close();
+	int x = -999999;
+
+	while (pow(3, x) <= R) {
+		x++;
+	}
+
+	ofstream outp;
+	outp.open("stepen.txt");
+	outp << (x - 1);
+	outp.close();
+}
